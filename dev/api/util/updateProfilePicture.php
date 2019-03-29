4fc50d4e45a9d180420bf472df0009d1de041f42
Joey Leger <legerinvestments@gmail.com>
2019-03-28 23:35:03 -0400

    latest build

--- dev/api/util/updateProfilePicture.php
+++ dev/api/util/updateProfilePicture.php
@@ -20,7 +20,7 @@
 	    $file_basename = substr($filename, 0, strripos($filename, '.')); // get file name
 	    $file_ext = substr($filename, strripos($filename, '.')); // get file extention
 		$newfilename = md5($file_basename) . $file_ext;
-		$newfilename = $_FILES['myFile']['tmp_name'];
+		//$newfilename = $_FILES['myFile']['tmp_name'];
         $url_to_uploads = 'public/uploads/';
 
         $userData = [
