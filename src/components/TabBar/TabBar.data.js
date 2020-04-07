1c1a4271d03ad9b532e6cbc600157afe3a932fc9
Joey <legerinvestments@gmail.com>
2020-04-06 20:57:20 -0400

    feat: tabbar looks less ugly

---
+++ src/components/TabBar/TabBar.data.js
@@ -0,0 +1,17 @@
+export const Data = [
+  {
+    href: '#todos',
+    controls: 'pills-todos',
+    name: 'Todos'
+  },
+  {
+    href: '#stats',
+    controls: 'pills-stats',
+    name: 'Stats'
+  },
+  {
+    href: '#profile',
+    controls: 'pills-profile',
+    name: 'Profile'
+  }
+];
--- src/components/TabBar/TabBar.js
+++ src/components/TabBar/TabBar.js
@@ -1,50 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Data } from './TabBar.data';
 import './TabBar.scss';
 
-const TabBar = () => (
-  <div className='tab-bar'>
-    {/* <ul class='nav justify-content-center nav-pills mb-3 todo-nav' id='pills-tab' role='tablist'>
-      <li class='nav-item'>
-        <a
-          class='nav-link active'
-          id='pills-todos-tab'
-          data-toggle='pill'
-          href='#pillTodos'
-          role='tab'
-          aria-controls='pills-home'
-          aria-selected='true'
-        >
-          Todos
-        </a>
-      </li>
-      <li class='nav-item'>
-        <a
-          class='nav-link'
-          id='pills-stats-tab'
-          data-toggle='pill'
-          href='#pillStats'
-          role='tab'
-          aria-controls='pills-profile'
-          aria-selected='false'
-        >
-          Stats
-        </a>
-      </li>
-      <li class='nav-item'>
-        <a
-          class='nav-link'
-          id='pills-profile-tab'
-          data-toggle='pill'
-          href='#pillProfile'
-          role='tab'
-          aria-controls='pills-contact'
-          aria-selected='false'
-        >
-          Profile
-        </a>
-      </li>
-    </ul> */}
-  </div>
-);
+const TabBar = () => {
+  const [selected, setSelected] = useState('pills-todos');
+
+  return (
+    <div className='tab-bar'>
+      <ul className='nav nav-pills tab-bar__nav' id='pills-tab' role='tablist'>
+        {Data.map(({ controls, href, name }, index) => (
+          <li className='tab-bar__item' key={index}>
+            <a
+              className={selected === controls ? 'nav-link active' : 'nav-link'}
+              id='pills-stats-tab'
+              data-toggle='pill'
+              href={href}
+              role='tab'
+              aria-controls={controls}
+              aria-selected={selected === controls}
+              onClick={() => setSelected(controls)}
+            >
+              {name}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default TabBar;
